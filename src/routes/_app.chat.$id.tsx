09ad1c56764c8d3314dc66b43c6cf5bfc134e307
rotamaisconnecta <rotamaisconnecta@gmail.com>
2026-08-08 11:11:30 -0300

    atualizar

--- src/routes/_app.chat.$id.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_app/chat/$id")({
-  loader: ({ params }) => {
-    throw redirect({ to: "/chat/$conversationId", params: { conversationId: params.id } });
-  },
-});
